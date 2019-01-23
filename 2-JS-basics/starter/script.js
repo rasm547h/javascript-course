aa98631f6b0af776f7537086b2c071d5e7d2fae5
Rasmus Larsson <Rasmus@RLL-PC>
2019-01-23 10:01:48 +0100

    script is modified to illustrate proper syntax for variables

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,18 @@
-console.log('Hello World!!!');
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+var fullAge = true;
+
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var invalidSyntax1 = 'First char is a number, 0 or 1 etc';
+var invalidSyntax2 = 'First char is a symbol, except $ and _';
+var invalidSyntax1 = 'variable name is a keyword, like if or function';
